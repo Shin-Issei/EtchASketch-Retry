27bc6542685de2f6b1b700aa1e97705838fead1a
Jordan <JMCollins0@aol.com>
2022-04-09 11:22:35 -0400

    Fucking finally!
    the grid creation function works as specified
    add grid creation prompt to customize grid size for user

--- script.js
+++ script.js
@@ -13,8 +13,28 @@
 // When the function is called and the grid items are created the styles of grid-tm-rs and -col will be added
 // according to the given arguments [DONE]
 
+const body = document.querySelector("body");
 const div = document.querySelector('.container');
+const btn = document.createElement("button");
+btn.textContent = "Make New Grid";
+body.appendChild(btn);
 
+function Grid (){
+    const newItem = document.createElement("div");
+    
+    div.appendChild(newItem);
+    
+    
+}
 
-div.setAttribute('style', `grid-template-columns: repeat(${3},1fr); grid-template-rows: repeat(${3},2fr`);
+function makeGrid (size)  {
+    div.setAttribute('style', `grid-template-columns: repeat(${size},1fr); 
+    grid-template-rows: repeat(${size},1fr`);
+    let i = 0;
+    while (i < size * size) {
+      Grid();
+      i++;
+    }
+  }
 
+btn.addEventListener("click", makeGrid (Number(prompt("Type a grid size"))));
